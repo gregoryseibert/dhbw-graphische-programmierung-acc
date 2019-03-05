d19a7bffc3b24584631c8e05d9ad0524034dfec4
rasple <frankme97@gmail.com>
2019-03-05 14:43:03 +0100

    removed clutter

--- Graph_Progr_CGen/tests/AllTests.cpp
+++
@@ -1,9 +0,0 @@
-// THIS IS A GENERATED FILE => DO NOT EDIT
-#include <gtest/gtest.h>
-#include "Etas_Ascet_Assertion.h"
-#include "EUnit_tests_SlopeTests_Automatic.cpp"
-
-int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
